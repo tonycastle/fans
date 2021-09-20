4a95374e02e44c9d3c4b969e5cb9147522e6ad88
tonycastle <castleantony6@gmail.com>
2021-09-20 04:51:16 +0200

    add login form validation and errors

--- src/Components/LandingPage/LoginForm.js
+++ src/Components/LandingPage/LoginForm.js
@@ -5,34 +5,45 @@ import http from "../../http-common";
 import { useState } from "react";
 import PasswordField from "./PasswordField";
 import { useHistory } from "react-router-dom";
+import * as yup from "yup";
 
 const LoginForm = ({ switchForm }) => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [loginError, setLoginError] = useState("");
+  const [formValues, setFormValues] = useState({ email: "", password: "" });
+  const [formErrors, setFormErrors] = useState({
+    email: false,
+    password: false,
+  });
+  const [loginError, setLoginError] = useState(false);
 
   let history = useHistory();
 
-  //TODO: figure out what to do with errors - probabky
-  //just display a message sayingincorrect credentials.
   const onSubmit = async (e) => {
     e.preventDefault();
     try {
-      let payload = { email, password };
+      let payload = { email: formValues.email, password: formValues.password };
       const result = await http.post("/api/users/login", payload);
       result.data.success
         ? history.push("/profile")
         : setLoginError(result.data.message);
     } catch (error) {
       console.log(error);
+      setLoginError(true); //use this to display some sorry something went wrong message
     }
   };
 
-  const handlePassword = (event) => {
-    setPassword(event.target.value);
+  const updateFormValues = (prop) => (event) => {
+    setFormValues({ ...formValues, [prop]: event.target.value });
   };
-  const handleEmail = (event) => {
-    setEmail(event.target.value);
+
+  let schema = {
+    email: yup.string().email().required(),
+    password: yup.string().length(6).required(),
+  };
+
+  const validateFormField = (field) => (e) => {
+    if (!schema[field].isValidSync(e.target.value)) {
+      setFormErrors({ ...formErrors, [field]: true });
+    }
   };
 
   return (
@@ -43,13 +54,34 @@ const LoginForm = ({ switchForm }) => {
             type="text"
             label="Email"
             variant="outlined"
-            onChange={handleEmail}
-            value={email}
+            onChange={updateFormValues("email")}
+            onFocus={() => setFormErrors({ ...formErrors, email: false })}
+            error={formErrors.email}
+            value={formValues.email}
             fullWidth
+            inputProps={{ "data-testid": "email-login" }}
+            required
+            helperText={
+              formErrors.email ? "Please enter a valid email address" : null
+            }
+            onBlur={validateFormField("email")}
           />
         </Grid>
         <Grid item>
-          <PasswordField value={password} onchange={handlePassword} />
+          <PasswordField
+            value={formValues.password}
+            onChange={updateFormValues("password")}
+            onBlur={validateFormField("password")}
+            onFocus={() => setFormErrors({ ...formErrors, password: false })}
+            error={formErrors.password}
+            inputProps={{ "data-testid": "password-login" }}
+            helperText={
+              formErrors.password
+                ? "Password must be at least 6 characters"
+                : null
+            }
+            required
+          />
         </Grid>
         <Grid item>
           <Button
@@ -58,6 +90,7 @@ const LoginForm = ({ switchForm }) => {
             fullWidth
             startIcon={<AddIcon />}
             type="submit"
+            data-testid="submit-login"
           >
             Sign in
           </Button>
@@ -65,7 +98,11 @@ const LoginForm = ({ switchForm }) => {
         </Grid>
         <Grid item>
           <p>Don't have an account yet?</p>
-          <Button className="switchForm" onClick={() => switchForm("signup")}>
+          <Button
+            className="switchForm"
+            data-testid="link-login"
+            onClick={() => switchForm("signup")}
+          >
             Sign Up
           </Button>
         </Grid>
